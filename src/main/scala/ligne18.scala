418626c576b917114bec77f5276f5dcf224815cc
Jean-Baptiste Daval <krokmou@crans.org>
2018-02-23 17:05:50 +0100

    Main Swing Window

--- src/main/scala/ligne18.scala
+++ src/main/scala/ligne18.scala
@@ -1,7 +1,11 @@
-object Ligne18
+import swing._
+
+object Ligne18 extends SimpleSwingApplication
 {
-    def main(args: Array[String]) : Unit =
-    {
-        println("Hello")
-    }
+  def top = new MainFrame
+  {
+    title = "Ligne 18"
+    preferredSize = new Dimension(800, 600)
+    contents = new Label("Ceci est la ligne 18... ou pas")
+  }
 }
