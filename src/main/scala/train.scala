6811bb5c1b8d6a7832bd54f41368b9b5dc36186f
Benjamin Graillot <graillot@crans.org>
2018-03-07 00:37:43 +0100

    Added Train

---
+++ src/main/scala/train.scala
@@ -0,0 +1,7 @@
+class Train(train_line :TrainLine, train_orientation :Boolean)
+{
+    var line = train_line
+    var orientation = train_orientation
+    var passengers = 0
+    var progress = 0.0f
+}
